d821c07f8aa4938af6f05b81e8dce77992455c25
Alexandra Besleaga <blah@blah.com>
2022-10-22 15:08:46 +0300

    Len function added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Welcome to the Band Name generator.")
-City = input("What's name of the city you grew up in? \n")
-Pet = input("What's your pet's name? \n")
-print ("Brand Name Generator is"+ " " + City + "" + Pet)
+#print("Welcome to the Band Name generator.")
+#City = input("What's name of the city you grew up in? \n")
+#Pet = input("What's your pet's name? \n")
+#print ("Brand Name Generator is"+ " " + City + "" + Pet)
+
+name = input("What is your name?\n")
+length = len (name)
+print(length)
