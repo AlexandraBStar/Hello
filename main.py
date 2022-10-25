d00b2ac71ed7ce125b9f0985476bf7f189236740
Alexandra Besleaga <blah@blah.com>
2022-10-25 18:04:05 +0300

    Day 1 - exercise 1.4

--- main.py
+++ main.py
@@ -10,17 +10,29 @@
 #Day1
 
 #exercise 1.1
-print('Day 1 - Python print function \nThe function is declared like this: \nprint("what to print")')
+#print('Day 1 - Python print function \nThe function is declared like this: \nprint("what to print")')
 
 #exercise 1.2
 #Fix the code below 👇
-print("Day 1 - String Manipulation")
-print("String Concatenation is done with the"+" "+"sign.")
-print('e.g. print("Hello" + "world")')
-print("New lines can be created with a backslash and n.")
+#print("Day 1 - String Manipulation")
+#print("String Concatenation is done with the"+" "+"sign.")
+#print('e.g. print("Hello" + "world")')
+#print("New lines can be created with a backslash and n.")
 
 #exercise 1.3
 #len function
-Name = input("Your name is:")
-lenght = len(Name)
-print("The lenght of your name is:", lenght, "characters")
+#Name = input("Your name is:")
+#lenght = len(Name)
+#print("The lenght of your name is:", lenght, "characters")
+
+#exercise 1.4
+
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
