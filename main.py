7610b4133520243d81da460ea5f424636c2d5641
Alexandra Besleaga <blah@blah.com>
2022-10-25 17:07:31 +0300

    Day 1 - exercise 1.1

--- main.py
+++ main.py
@@ -3,6 +3,13 @@
 #Pet = input("What's your pet's name? \n")
 #print ("Brand Name Generator is"+ " " + City + "" + Pet)
 
-name = input("What is your name?\n")
-length = len (name)
-print(length)
+#name = input("What is your name?\n")
+#length = len (name)
+#print(length)
+
+#Day1 - exercise 1
+
+proposition_one = "Day 1 - Python print function"
+proposition_two = "The function is declared like this:"
+proposition_three = "print('what to print')"
+print("Day 1 - Python print function \nThe function is declared like this: \nprint('what to print')")
